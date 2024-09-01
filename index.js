8f2cc2ea91a2d34d7f39cbb023bd9d661f63967d
megbit <154680519+megb1t@users.noreply.github.com>
2024-09-01 10:30:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const button = document.getElementById('contentDrop');
+
+const displayDrop = () =>{
+    button.style.display="grid"
+
+}
+
+const closeNav = () =>{
+    button.style.display="none"
+}
