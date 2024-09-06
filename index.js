634a600ef8c28b90b399bb100ae32a3eb949fd9b
megbit <154680519+megb1t@users.noreply.github.com>
2024-09-06 17:57:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const button = document.getElementById('contentDrop');
+const button = document.getElementById('ezNavSect');
+const phoneNav = document.getElementById('contentDropPhone');
+
+const phoneButton = document.getElementById('phoneNavThree')
 
 const displayDrop = () =>{
     button.style.display="grid"
 
 }
 
+const displayDropPhone = () =>{
+    phoneNav.style.display="block"
+
+}
+
 const closeNav = () =>{
     button.style.display="none"
+}
+const closeNavPhone = () =>{
+    phoneNav.style.display="none"
+}
+
+const phoneOpenNav = () =>{
+    phoneButton.style.width="40vw"
+}
+
+const phoneCloseNav = () =>{
+    phoneButton.style.width="0"
 }
